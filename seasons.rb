0ace33b818802485f327f64a600dd69a8ccdbf30
Sutee Dee <thaiyoshi@Sutees-MacBook-Pro.local>
2013-06-17 21:14:31 -0700

    first commit for season one-use app

---
+++ seasons.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+require 'Geocoder'
+
+# Apricots, Arugula, Beets, Cherries , Garlic, Greens, Herbs, Lettuce, Onions , Peas, Radishes, Salad Greens, Spinach, Summer squash
+ingredient_by_month = {'June' => {'San Francisco'=> ['apricots', 'arugula']}}
+
+get '/' do 
+	results = Geocoder.search("McCarren Park, Brooklyn, NY")
+	r = results.first
+	state = r.state
+	month = Date::MONTHNAMES[Date.today.month]
+	ingredient_by_month[month]['San Francisco'].join(", ")
+	# request.location
+end
