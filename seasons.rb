9235f9e53ae426c77526d65bca57d8308ca1b1d3
Sutee Dee <thaiyoshi@Sutees-MacBook-Pro.local>
2013-06-18 00:19:27 -0700

    test request.location.state

--- seasons.rb
+++ seasons.rb
@@ -3,13 +3,10 @@ require 'sinatra'
 require 'geocoder'
 
 # Apricots, Arugula, Beets, Cherries , Garlic, Greens, Herbs, Lettuce, Onions , Peas, Radishes, Salad Greens, Spinach, Summer squash
-ingredient_by_month = {'June' => {'San Francisco'=> ['apricots', 'arugula']}}
+ingredient_by_month = {'June' => {'California'=> ['apricots', 'arugula']}}
 
 get '/' do 
-	results = Geocoder.search("McCarren Park, Brooklyn, NY")
-	r = results.first
-	state = r.state
+	state = r.request.location.state
 	month = Date::MONTHNAMES[Date.today.month]
-	ingredient_by_month[month]['San Francisco'].join(", ")
-	request.location.state
+	ingredient_by_month[month][state].join(", ")
 end
