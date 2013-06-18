231911fc1ff883e99adbc3f04bbcdf28e60358f1
Sutee Dee <thaiyoshi@Sutees-MacBook-Pro.local>
2013-06-18 00:42:08 -0700

    more test data

--- seasons.rb
+++ seasons.rb
@@ -2,11 +2,14 @@ require 'rubygems'
 require 'sinatra'
 require 'geocoder'
 
-# Apricots, Arugula, Beets, Cherries , Garlic, Greens, Herbs, Lettuce, Onions , Peas, Radishes, Salad Greens, Spinach, Summer squash
-ingredient_by_month = {'June' => {'California'=> ['apricots', 'arugula']}}
+#test data
+ingredient_by_month_and_state = {'June' => 
+									{'California' => ['Fennel' , 'Figs', 'Garlic', 'Green Garlic' , 'Herbs', 'Horseradish', 'Kale' , 'Kohlrabi', 'Leeks', 'Lemons', 'Lettuces' , 'Loquats', 'Melons', 'Mushrooms' , 'Mustard Greens', 'Nectarines', 'Nettles' , 'Okra', 'Olives', 'Onions', 'Oranges', 'Peaches', 'Peas', 'Peppers', 'Pistachioes', 'Plums', 'Pluots', 'Purslane' , 'Radicchio', 'Radish' , 'Rapini' , 'Raspberries', 'Rhubarb', 'Scallions', 'Shallots', 'Spinach', 'Strawberries' , 'Summer squash', 'Tayberries', 'Tomatillos', 'Tomatoes', 'Walnuts']}
+									{'Georgia' => ['Bell Pepper', 'Blueberries', 'Carrots', 'Cucumbers', 'Field Peas', 'Grapes', 'Greens', 'Melons', 'Peaches', 'Snap Beans', 'Sweet Corn', 'Tomatoes', 'Vidalia Onions', 'Yellow Squash', 'Zucchini']}
+								}
 
 get '/' do 
-	state = request.location.state
-	month = Date::MONTHNAMES[Date.today.month]
+	state = request.location.state #based on IP, via geocoder
+	month = Date::MONTHNAMES[Date.today.month] #month name
 	ingredient_by_month[month][state].join(", ")
 end
