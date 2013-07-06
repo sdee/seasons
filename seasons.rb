35577b146165a2335c008f24d1b10d119779b7a0
Sutee Dee <thaiyoshi@Sutees-MacBook-Pro.local>
2013-07-06 00:20:32 -0700

    default north for large states

--- seasons.rb
+++ seasons.rb
@@ -1,16 +1,19 @@
 require 'rubygems'
 require 'sinatra'
 require 'geocoder'
+require 'json'
+
 require_relative "flickr"
 
 # include HTTParty
 
-	ingredient_by_month_and_state = {'June' => 
-									{'California' => ['Fennel' , 'Figs', 'Garlic', 'Green Garlic' , 'Herbs', 'Horseradish', 'Kale' , 'Kohlrabi', 'Leeks', 'Lemons', 'Lettuces' , 'Loquats', 'Melons', 'Mushrooms' , 'Mustard Greens', 'Nectarines', 'Nettles' , 'Okra', 'Olives', 'Onions', 'Oranges', 'Peaches', 'Peas', 'Peppers', 'Pistachioes', 'Plums', 'Pluots', 'Purslane' , 'Radicchio', 'Radish' , 'Rapini' , 'Raspberries', 'Rhubarb', 'Scallions', 'Shallots', 'Spinach', 'Strawberries', 
-										'Summer squash', 'Tayberries', 'Tomatillos', 'Tomatoes', 'Walnuts'],
-										'Georgia' => ['Bell Pepper', 'Blueberries', 'Carrots', 'Cucumbers', 'Field Peas', 'Grapes', 'Greens', 'Melons', 'Peaches', 'Snap Beans', 'Sweet Corn', 'Tomatoes', 'Vidalia Onions', 'Yellow Squash', 'Zucchini']}
-									}
+	# ingredient_by_month_and_state = {'July' => 
+	# 								{'California' => ['Fennel' , 'Figs', 'Garlic', 'Green Garlic' , 'Herbs', 'Horseradish', 'Kale' , 'Kohlrabi', 'Leeks', 'Lemons', 'Lettuces' , 'Loquats', 'Melons', 'Mushrooms' , 'Mustard Greens', 'Nectarines', 'Nettles' , 'Okra', 'Olives', 'Onions', 'Oranges', 'Peaches', 'Peas', 'Peppers', 'Pistachioes', 'Plums', 'Pluots', 'Purslane' , 'Radicchio', 'Radish' , 'Rapini' , 'Raspberries', 'Rhubarb', 'Scallions', 'Shallots', 'Spinach', 'Strawberries', 
+	# 									'Summer squash', 'Tayberries', 'Tomatillos', 'Tomatoes', 'Walnuts'],
+	# 									'Georgia' => ['Bell Pepper', 'Blueberries', 'Carrots', 'Cucumbers', 'Field Peas', 'Grapes', 'Greens', 'Melons', 'Peaches', 'Snap Beans', 'Sweet Corn', 'Tomatoes', 'Vidalia Onions', 'Yellow Squash', 'Zucchini']}
+	# 								}
 
+	ingredient_by_month_and_state = JSON.parse( IO.read("./seasons.json") )
 
 	helpers do
 		def test
@@ -26,8 +29,24 @@ require_relative "flickr"
 			@state = request.location.state #based on IP, via geocoder
 		end
 		@month = Date::MONTHNAMES[Date.today.month] #month name
-		@ingredients = ingredient_by_month_and_state[@month][@state].join(", ")
-		@test = test
+		day = Date.today.day
+		if day >15
+			timing = "late"
+		else
+			timing = "early"
+		end
+		key = @month+'-'+timing
+
+		month_info = ingredient_by_month_and_state[key]
+
+		if month_info.has_key?(@state)
+			@ingredients = month_info[@state].join(", ")
+		elsif month_info.has_key?(@state+" (Northern)")
+			@ingredients = month_info[@state+" (Northern)"].join(", ")
+		else
+			@ingredients = "sorry no info"
+		end
+		
 		#puts flickr_ingred_photo('arugula')
 		erb :index
 	end
