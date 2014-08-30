051d8d4e587f4d9f523d2c21743d24a68d168db1
sdee <Dee@Sutees-MacBook-Air.local>
2014-08-30 13:30:06 -0700

    base url now redirects to restful url based on month and state

--- seasons.rb
+++ seasons.rb
@@ -14,7 +14,7 @@ require_relative "flickr"
 # 									'Georgia' => ['Bell Pepper', 'Blueberries', 'Carrots', 'Cucumbers', 'Field Peas', 'Grapes', 'Greens', 'Melons', 'Peaches', 'Snap Beans', 'Sweet Corn', 'Tomatoes', 'Vidalia Onions', 'Yellow Squash', 'Zucchini']}
 # 								}
 
-ingredient_by_month_and_state = JSON.parse( IO.read("./seasons.json") )
+ingredient_by_month_and_state = JSON.parse(IO.read("./seasons.json"))
 
 helpers do
 	def test
@@ -26,38 +26,65 @@ def get_state (ip)
 	@state = ip == '127.0.0.1' ? 'California' : @state = request.location.state #based on IP, via geocoder
 end
 
-def get_timing ()
+def get_timing
 	month = Date::MONTHNAMES[Date.today.month] #month name
 	day = Date.today.day
-	timing = day > 15 ? "late" : "early"
-	timing = month+'-'+timing
-	return timing, month
+	earlylate = day > 15 ? "Late" : "Early"
+	timing = month+'-'+earlylate
+	return timing, month, earlylate
 end
 
-def get_seasonal_ingredients (timing, state, ingredient_by_month_and_state)
-	month_info = ingredient_by_month_and_state[timing]
-	if month_info.has_key?(state)
-		ingredients = month_info[state]
-		#default to northern for states with northern and southern
-	elsif month_info.has_key?(state+" (Northern)")
-		ingredients = month_info[state+" (Northern)"]
+def north (state)
+	state+" (Northern)"
+end
+
+def early (month)
+	month+"-early"
+end
+
+def partofmonth (month, earlylate)
+	month+"-"+earlylate.downcase
+end
+
+#Looks up ingredients by time and location.
+#Takes in either a half month (early or late) or a month in which case we default to early part of the month
+#Some states are divided into the Northern and Southern halves. For now, we default to the Northern half.
+def get_seasonal_ingredients (timing, state, ingred_map)
+	if ingred_map.has_key?(timing)
+		ingreds_by_state = ingred_map[timing]
+	else
+		ingreds_by_state = ingred_map[early(timing)] #for months default to early
+	end
+	if ingreds_by_state.has_key?(state)
+		ingredients = ingreds_by_state[state]
+	elsif ingreds_by_state.has_key?(north(state))
+		ingredients = ingreds_by_state [north(state)]
 	else
 		ingredients = "sorry no info"
 	end
-	ingredients.join(", ")
 end
 
-get '/' do
-	@state = get_state(request.ip.to_s)
-	@timing, @month = get_timing
-	@ingredients = get_seasonal_ingredients(@timing, @state, ingredient_by_month_and_state)
+#Routes with most specific first
+get '/:state/:month/:earlylate' do
+	@state = params[:state]
+	@month = params[:month]
+	@earlylate = params[:earlylate]
+	timing = partofmonth(@month, @earlylate)
+	@ingredients = get_seasonal_ingredients(timing, @state, ingredient_by_month_and_state)
 	erb :index
 end
 
 get '/:state/:month' do
 	@state = params[:state]
 	@month = params[:month]
-	@timing = @month+'-'+"early"
-	@ingredients = get_seasonal_ingredients(@timing, @state, ingredient_by_month_and_state)
+	@ingredients = get_seasonal_ingredients(@month, @state, ingredient_by_month_and_state)
 	erb :index
+end
+
+get '/' do
+	state = get_state(request.ip.to_s)
+	timing, month, earlylate = get_timing
+	# @ingredients = get_seasonal_ingredients(@timing, @state, ingredient_by_month_and_state)
+	# erb :index
+	redirect to('/'+state+'/'+month+'/'+earlylate)
 end
